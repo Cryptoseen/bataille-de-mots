806c8058ede45900aec13f0481ead648449643b9
Aimee RITLENG <aimeeritleng@outlook.com>
2022-02-23 20:21:57 +0100

    fix(user); get random username button

--- src/WordleWars.vue
+++ src/WordleWars.vue
@@ -154,6 +154,12 @@ function updateGameStage (stage: GameState) {
   }
 }
 
+function updateUserName(){
+  let newUsername = getRandomUsername()
+  localStorage.setItem('username', newUsername.randomUsername)
+  username = newUsername.randomUsername
+}
+
 // Returns true if every user is in one of the `stages`
 function allInStages (stages: GameState[]) {
   if (!others?.value || !others?.value.count) {
@@ -254,7 +260,7 @@ function createEmojiScore (successGrid: string) {
             <label for="set-username">Je suis ...</label>
             <div class="flex items-center">
               <input type="text" id="set-username" v-model="username" autocomplete="off" required />
-              <button type="button" class="mt-0 ml-2 button-simple" @click="getRandomUsername()">
+              <button type="button" class="mt-0 ml-2 button-simple" @click="updateUserName()">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                   <path d="M13.5 2c-5.621 0-10.211 4.443-10.475 10h-3.025l5 6.625 5-6.625h-2.975c.257-3.351 3.06-6 6.475-6 3.584 0 6.5 2.916 6.5 6.5s-2.916 6.5-6.5 6.5c-1.863 0-3.542-.793-4.728-2.053l-2.427 3.216c1.877 1.754 4.389 2.837 7.155 2.837 5.79 0 10.5-4.71 10.5-10.5s-4.71-10.5-10.5-10.5z"/>
                 </svg>
